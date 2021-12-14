e432fd25223b749376574537df9ede45d9b0302f
Vaisakh Murali <mvaisakh@statixos.com>
2021-12-14 21:23:00 +0600

    st: Initial optimsation setup
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- config.mk
+++ config.mk
@@ -21,6 +21,7 @@ LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
        `$(PKG_CONFIG) --libs freetype2`
 
 # flags
+CFLAGS = -flto -mtune=native -O3
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
 STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
 STLDFLAGS = $(LIBS) $(LDFLAGS)
